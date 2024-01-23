8d58020f551dc5f794b59aa4e1204a55f2e2984a
Punnyman21 <nathandj21@gmail.com>
2024-01-23 02:25:54 -0800

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,11 @@
+const myHousehold = 2
+const myHome = 7
+const myFood = 22
+const myWater = 2
+const myPurchases = 4
+const myWaste = 30
+const myRecycle = 14
+const myTransportation = 10
+const total = myHousehold + myHome + myFood + myWater + myPurchases + myWaste + myRecycle + myRecycle 
+const myHeading = document.querySelector("h2");
+myHeading.textContent = "91";
