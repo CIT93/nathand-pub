dc63eeb41b67582b319584b3e994b51c9080c31b
Punnyman21 <nathanjd21@gmail.com>
2024-01-31 14:07:43 -0800

    functions

--- w3/main.js
+++ w3/main.js
@@ -1,24 +1,29 @@
-let carbonFootPoints = 0;
-const numberInHouseHold = 1;
-if (numberInHouseHold === 1) {
+function determineHouseHoldpts() {
+  if (numberInHouseHold === 1) {
     carbonFootPoints = carbonFootPoints + 14;
-} else if (numberInHouseHold === 2) {
+  } else if (numberInHouseHold === 2) {
     carbonFootPoints = carbonFootPoints + 12;
-} 
-else if (numberInHouseHold === 3) {
+  } else if (numberInHouseHold === 3) {
     carbonFootPoints = carbonFootPoints + 10;
-} 
-else if (numberInHouseHold === 4) {
+  } else if (numberInHouseHold === 4) {
     carbonFootPoints = carbonFootPoints + 8;
-} 
-else if (numberInHouseHold === 5) {
+  } else if (numberInHouseHold === 5) {
     carbonFootPoints = carbonFootPoints + 6;
-}
-else if (numberInHouseHold === 6) {
+  } else if (numberInHouseHold === 6) {
     carbonFootPoints = carbonFootPoints + 4;
-}
-else if(numberInHouseHold > 6) {
+  } else if (numberInHouseHold > 6) {
     carbonFootPoints = carbonFootPoints + 2;
+  }
+
+  console.log(
+    `based on the number of member of the household of ${numberInHouseHold} the points would be ${carbonFootPoints}.`
+  );
 }
 
-console.log(`based on the number of member of the household of ${numberInHouseHold} the points would be ${carbonFootPoints}.`)
+let carbonFootPoints = 0;
+const numberInHouseHold = 1;
+
+//global scope
+
+determineHouseHoldpts(3)
+determineHouseHoldpts(4)
